e4d4c813c7374bf39b70051f3fcfbcb27f4a8a0a
engrcruz1 <rolandcruzjr@outlook.com>
2025-02-11 12:00:09 +0800

    Added src foldr and other project files

---
+++ src/main/java/com/training/week1/HelloWorld.java
@@ -0,0 +1,8 @@
+// src/main/java/com/training/week1/HelloWorld.java
+package com.training.week1;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Java Version: " + System.getProperty("java.version"));
+    }
+}
